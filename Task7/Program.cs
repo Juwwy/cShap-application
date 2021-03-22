61fec9f81d17a1eb2eee4c26e5bfe9b20a8253c9
Olowu Juwon <juwonolowu52@gmail.com>
2021-03-22 23:21:17 +0100

    C# application

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -6,13 +6,29 @@ namespace Task7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter hourly rate:");
+            Console.WriteLine("Employee1=> Enter hourly rate:");
             double hourlyRate = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter hour worked:");
+            Console.WriteLine("Employee1=> Enter hour worked:");
             int hourWorked = int.Parse(Console.ReadLine());
 
-            Console.Write("Pay for Employee1 is $ " + hourlyRate * hourWorked);
+            Console.WriteLine("Pay for Employee1 is $ " + hourlyRate * hourWorked);
+
+             Console.WriteLine("Employee2=> Enter hourly rate:");
+            double hourlyRate2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Employee2=> Enter hour worked:");
+            int hourWorked2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pay for Employee1 is $ " + hourlyRate2 * hourWorked2);
+
+             Console.WriteLine("Employee3=> Enter hourly rate:");
+            double hourlyRate3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Employee3=> Enter hour worked:");
+            int hourWorked3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Pay for Employee1 is $ " + hourlyRate3 * hourWorked3);
         }
     }
 }
